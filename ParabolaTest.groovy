a28c5af994e8219dbf80971a3684da55aef911ef
Maura Hunter <Maura Hunter@10.0.0.146>
2023-09-12 17:34:11 -0400

    I chnaged one of the cones, well two of them

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -1,10 +1,10 @@
 import eu.mihosoft.vrl.v3d.Parabola
 
-return [ Parabola.extrudeByEquation(5,0.27,0,1)
-	.move(20,0,0),
-	Parabola.coneByEquation(5,1.27,0.7)
+return [ Parabola.extrudeByEquation(5,0.207,0,1)
+	.move(100,0,0),
+	Parabola.coneByEquation(5,1.107,0.7)
 	.move(30,0,0),
 	Parabola.coneByFocalLength(10, 10).move(50,0,0),
-	Parabola.cone(10, 20), // smooth parabolic cone
+	Parabola.cone(10, 200), // smooth parabolic cone
 	Parabola.cone(10, 60,3).movex(-30)// Pointy parabolic cone		
 ]
